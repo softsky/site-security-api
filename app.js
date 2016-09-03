284686fdf15534aac7d5b9eede999d9dc2549cde
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-09-03 08:47:26 +0300

    adding some clearity to use of enviroment variables

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ var https = require('https');
 var privateKey  = fs.readFileSync(__dirname + '/sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync(__dirname + '/sslcert/server.crt', 'utf8');
 
+var options = require('./src/options.json');
+var [protocol, host, port] = (process.env.MONGODB_PORT || "tcp://localhost:27017").split(/\:/);
+var report_path = process.env.REPORT_PATH || '/data';
+options.mongo.host = host.replace(/\/\//,''); options.mongo.port = port; //FIXME use destructuring assignments
+
+options.report_path = process.env.NODE_ENV === 'production'?report_path:`${report_path}/${process.env.SCAN_DOMAIN}`;
+console.log(options.mongo);
+
 var seneca = require('seneca')()
 // .use('src/queue')
 // .use('src/email')
 // .use('src/routes')
-	.use('src/entity')
-	.use('src/exec');
+	.use('src/entity', options)
+	.use('src/exec', options);
 	// .listen({
 	//     type: 'http',
 	//     port: '3000',
